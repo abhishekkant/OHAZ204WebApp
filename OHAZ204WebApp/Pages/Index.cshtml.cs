526e9002256fe1ca80383e5894dfc1c97286f40f
Abhishek Kant <abhishek@abhishekkant.net>
2020-09-01 19:03:51 +0530

    Add project files.

---
+++ OHAZ204WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OHAZ204WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public IndexModel(IConfiguration config, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public void OnGet()
+        {
+            ViewData["name"] = _config["Name"];
+        }
+    }
+}
